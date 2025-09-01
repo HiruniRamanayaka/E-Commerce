4a4321fe9a1026335d4c65a016f05862ca6b152a
Hiruni Ramanayaka <hiruniramanayaka9@gmail.com>
2025-09-02 04:11:02 +0530

    Add login/logout buttons

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
 import { useCart } from "../context/CartContext.jsx";
+import { LoginButton } from "./LoginButton.jsx";
+import { LogoutButton } from "./LogoutButton.jsx";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
   const { cartCount } = useCart();
+  const { isAuthenticated, user } = useAuth0();
+
   return (
-    <nav style={{ padding: "1rem", background: "#eee" }}>
-      <h1>My Shop</h1>
-      <Link to="/">Home</Link>          {/* Go to Home */}
-      <Link to="/cart">Cart ({cartCount})</Link>   {/* Go to Cart */}
+    <nav style={{ padding: "1rem", background: "#eee", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+      <div>
+        <h1>My Shop</h1>
+        <Link to="/">Home</Link>{" | "}
+        <Link to="/cart">Cart ({cartCount})</Link>
+      </div>
+
+      <div>
+        {isAuthenticated ? (
+          <>
+            <span style={{ marginRight: "1rem" }}>
+              Hi, {user?.name || "User"}
+            </span>
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
     </nav>
   );
 };
