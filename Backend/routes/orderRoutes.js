2f33f10bb72a21ece89ae8ecd05a6148604bbd9d
Hiruni Ramanayaka <hiruniramanayaka9@gmail.com>
2025-09-02 04:17:29 +0530

    Validate order routes

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,14 +1,18 @@
 import express from "express";
 import Order from "../models/Order.js";
+import { checkJwt } from "../middleware/authMiddleware.js";
 import { validateOrder } from "../middleware/validateMiddleware.js";
 
 const router = express.Router();
 
 // POST /api/orders → create new order
-router.post("/", validateOrder, async (req, res) => {
-    console.log("Received order:", req.body);  // 🔹 log incoming request
+router.post("/", checkJwt, validateOrder, async (req, res) => {
+  // console.log("Received order:", req.body);  // 🔹 log incoming request
   try {
-    const order = await Order.create(req.body);
+    const order = await Order.create({
+      ...req.body,
+      owner: req.auth.sub, // store user id from token
+    });
     res.status(201).json(order);
   } catch (err) {
     console.error(err);
